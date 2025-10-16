6a954a65667394eed6bd6bdd370dfe29af4c19aa
Naman230804 <149310925+Naman230804@users.noreply.github.com>
2025-10-17 00:29:25 +0530

    Time: 7 ms (6.71%), Space: 97.5 MB (25.65%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int n = prices.size();
+        int buy = prices[0];
+        for(int i=1;i<n;i++){
+            if(buy > prices[i]){
+                buy = prices[i];
+            }
+            ans = max(ans, prices[i] - buy);
+        }
+
+        return ans;
+    }
+};
